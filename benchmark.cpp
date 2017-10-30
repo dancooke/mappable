63dedf465fb0e0bb92192abe431918870547f63a
Daniel Cooke <dcooke@well.ox.ac.uk>
2017-10-30 12:43:04 +0000

    Improve benchmarks
    Clear memory after use for IntervalTree

--- benchmark.cpp
+++ benchmark.cpp
@@ -87,92 +87,112 @@ auto generate_interval_tree_data(const std::vector<Read>& reads)
 
 int main()
 {
-    using D = std::chrono::microseconds;
-    
-    constexpr std::size_t num_reads {2'000'000};
+    constexpr std::size_t num_reads {10'000'000};
     constexpr unsigned read_size {150};
-    constexpr ContigRegion::Size contig_size {5'000'000};
+    constexpr ContigRegion::Size contig_size {50'000'000};
     
-    std::cout << "Generating mappable data" << std::endl;
+    std::cout << "Generating test data..." << std::endl;
     auto mappable_data = generate_mappable_data(num_reads, read_size, contig_size);
-    std::cout << "Generating interval tree data" << std::endl;
     auto interval_tree_data = generate_interval_tree_data(mappable_data);
-    
-    std::cout << "Starting benchmarks" << std::endl;
     
     const ContigRegion small_test_region {contig_size / 2 - 200, contig_size / 2 + 200};
     const ContigRegion big_test_region {contig_size / 2 - contig_size / 4, contig_size / 2 + contig_size / 4};
     
+    using D = std::chrono::microseconds;
     auto start = std::chrono::system_clock::now();
-    IntervalTree<std::string> tree {interval_tree_data}; // passes by ref
-    auto end = std::chrono::system_clock::now();
+    auto end = start;
     auto duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "IntervalTree<std::string> constructed in " << duration.count() << "ms" << std::endl;
     
-    start = std::chrono::system_clock::now();
-    std::vector<IntervalTreeRead> interval_tree_overlapping_small;
-    tree.findOverlapping(small_test_region.begin(), small_test_region.end(), interval_tree_overlapping_small);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "interval_tree_overlapping_small.size() = " << interval_tree_overlapping_small.size() 
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
+    std::cout << "Starting benchmarks..." << std::endl;
     
-    start = std::chrono::system_clock::now();
-    std::vector<IntervalTreeRead> interval_tree_contained_small;
-    tree.findContained(small_test_region.begin(), small_test_region.end(), interval_tree_contained_small);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "interval_tree_contained_small.size() = " << interval_tree_contained_small.size() 
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
+    //
+    // IntervalTree tests
+    //
+    {
+        start = std::chrono::system_clock::now();
+        IntervalTree<std::string> tree {interval_tree_data}; // passes by ref
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "IntervalTree<std::string> constructed in " << duration.count() << "ms" << std::endl;
+        
+        start = std::chrono::system_clock::now();
+        std::vector<IntervalTreeRead> interval_tree_overlapping_small;
+        tree.findOverlapping(small_test_region.begin(), small_test_region.end(), interval_tree_overlapping_small);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "interval_tree_overlapping_small.size() = " << interval_tree_overlapping_small.size() 
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        interval_tree_overlapping_small.clear();
+        interval_tree_overlapping_small.shrink_to_fit();
+        
+        start = std::chrono::system_clock::now();
+        std::vector<IntervalTreeRead> interval_tree_contained_small;
+        tree.findContained(small_test_region.begin(), small_test_region.end(), interval_tree_contained_small);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "interval_tree_contained_small.size() = " << interval_tree_contained_small.size() 
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        interval_tree_contained_small.clear();
+        interval_tree_contained_small.shrink_to_fit();
+        
+        start = std::chrono::system_clock::now();
+        std::vector<IntervalTreeRead> interval_tree_overlapping_big;
+        tree.findOverlapping(big_test_region.begin(), big_test_region.end(), interval_tree_overlapping_big);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "interval_tree_overlapping_big.size() = " << interval_tree_overlapping_big.size() 
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        interval_tree_overlapping_big.clear();
+        interval_tree_overlapping_big.shrink_to_fit();
+        
+        start = std::chrono::system_clock::now();
+        std::vector<IntervalTreeRead> interval_tree_contained_big;
+        tree.findContained(big_test_region.begin(), big_test_region.end(), interval_tree_contained_big);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "interval_tree_contained_big.size() = " << interval_tree_contained_big.size() 
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        interval_tree_contained_big.clear();
+        interval_tree_contained_big.shrink_to_fit();
+    }
     
-    start = std::chrono::system_clock::now();
-    std::vector<IntervalTreeRead> interval_tree_overlapping_big;
-    tree.findOverlapping(big_test_region.begin(), big_test_region.end(), interval_tree_overlapping_big);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "interval_tree_overlapping_big.size() = " << interval_tree_overlapping_big.size() 
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
-    
-    start = std::chrono::system_clock::now();
-    std::vector<IntervalTreeRead> interval_tree_contained_big;
-    tree.findContained(big_test_region.begin(), big_test_region.end(), interval_tree_contained_big);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "interval_tree_contained_big.size() = " << interval_tree_contained_big.size() 
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
-    
-    start = std::chrono::system_clock::now();
-    ReadSet set {std::make_move_iterator(std::begin(mappable_data)), 
-                 std::make_move_iterator(std::end(mappable_data))};
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "ReadSet constructed in " << duration.count() << "ms" << std::endl;
-    
-    start = std::chrono::system_clock::now();
-    const auto mappable_overlapping_small = overlap_range(set, small_test_region);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "size(mappable_overlapping_small) = " << size(mappable_overlapping_small)
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
-    
-    start = std::chrono::system_clock::now();
-    const auto mappable_contained_small = contained_range(set, small_test_region);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "size(mappable_contained_small) = " << size(mappable_contained_small)
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
-    
-    start = std::chrono::system_clock::now();
-    const auto mappable_overlapping_big = overlap_range(set, big_test_region);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "size(mappable_overlapping_big) = " << size(mappable_overlapping_big)
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
-    
-    start = std::chrono::system_clock::now();
-    const auto mappable_contained_big = contained_range(set, big_test_region);
-    end = std::chrono::system_clock::now();
-    duration = std::chrono::duration_cast<D>(end - start);
-    std::cout << "size(mappable_contained_big) = " << size(mappable_contained_big)
-              << ". Calculated in " << duration.count() << "ms" << std::endl;
+    //
+    // Mappable tests
+    //
+    {
+        start = std::chrono::system_clock::now();
+        ReadSet set {std::make_move_iterator(std::begin(mappable_data)), 
+                     std::make_move_iterator(std::end(mappable_data))};
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "ReadSet constructed in " << duration.count() << "ms" << std::endl;
+        
+        start = std::chrono::system_clock::now();
+        const auto mappable_overlapping_small = overlap_range(set, small_test_region);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "size(mappable_overlapping_small) = " << size(mappable_overlapping_small)
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        
+        start = std::chrono::system_clock::now();
+        const auto mappable_contained_small = contained_range(set, small_test_region);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "size(mappable_contained_small) = " << size(mappable_contained_small)
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        
+        start = std::chrono::system_clock::now();
+        const auto mappable_overlapping_big = overlap_range(set, big_test_region);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "size(mappable_overlapping_big) = " << size(mappable_overlapping_big)
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+        
+        start = std::chrono::system_clock::now();
+        const auto mappable_contained_big = contained_range(set, big_test_region);
+        end = std::chrono::system_clock::now();
+        duration = std::chrono::duration_cast<D>(end - start);
+        std::cout << "size(mappable_contained_big) = " << size(mappable_contained_big)
+                  << ". Calculated in " << duration.count() << "ms" << std::endl;
+    }
 }
